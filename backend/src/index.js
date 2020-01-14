c55a59cf2fdbc996d1fb3712934b23b975e5d54d
leonardo-costa <leown.dev@gmail.com>
2020-01-14 18:32:39 -0300

    feat:add mongodb,dotenv config

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,12 +1,19 @@
+require('dotenv').config();
+
 const express = require('express');
+const mongoose = require('mongoose');
 
 const app = express();
 
 const PORT = process.env.PORT || 3333;
 
-app.get('/', (req, res) => {
-  return res.json({ ok: true });
-});
+mongoose.connect(
+  `mongodb+srv://${process.env.MONGODB_USER_LOGIN}:${process.env.MONGODB_USER_PASSWORD}@${process.env.MONGODB_URL}?retryWrites=true&w=majority`,
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  }
+);
 
 app.listen(PORT, () => {
   console.log(`App running on port: ${PORT}`);
