20e722c0dfcd13fcb8ac70b9074d3b77995c0978
leonardo-costa <leown.dev@gmail.com>
2020-01-14 18:36:37 -0300

    feat:add routes.js

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 
 const express = require('express');
 const mongoose = require('mongoose');
+const routes = require('./routes');
 
 const app = express();
 
@@ -14,6 +15,9 @@ mongoose.connect(
     useUnifiedTopology: true,
   }
 );
+
+app.use(express.json());
+app.use(routes);
 
 app.listen(PORT, () => {
   console.log(`App running on port: ${PORT}`);
